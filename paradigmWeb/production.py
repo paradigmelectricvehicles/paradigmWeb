615881dd4295dcf06ca1b209036632e6ac2d61ff
hsntrq <mohdhasantariq@gmail.com>
2024-03-25 14:46:31 +0500

    cors

--- paradigmWeb/production.py
+++ paradigmWeb/production.py
@@ -15,13 +15,12 @@ DATABASES["default"] = {
     'PORT': env("DB_PORT"),
 }
 
-ALLOWED_HOSTS = ["127.0.0.1"] + env("API_GATEWAY").split(",")
+ALLOWED_HOSTS = ["127.0.0.1"] + env("API_GATEWAY", "").split(",")
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 SECRET_KEY = env("SECRET_KEY")
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000'] + env("CORS_ORIGIN_WHITELIST").split(",")
-
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000'] + env("CORS_ORIGIN_WHITELIST", "").split(",")
 # SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
